1becec2fcf9971a81d1b6417cb4e4a06cd94ea30
Tomas <jtml.mass@gmail.com>
2019-09-26 01:48:57 -0400

    Completar acapite A sobre lineas totales
    Indicar la cantidad de lineas del recurso retornado.

--- src/main/java/tarea1.java
+++ src/main/java/tarea1.java
@@ -9,5 +9,9 @@ public class tarea1 {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://www.wikihow.com/Play-Guitar").get();
         System.out.println("Titulo: " + doc.title());
+
+        //Parte A Indicar la cantidad de lineas del recurso retornado
+        int lineas = doc.html().split(System.getProperty("line.separator")).length;
+        System.out.println("Cantidad de lineas en el Recurso retornado: " + lineas);
     }
 }
