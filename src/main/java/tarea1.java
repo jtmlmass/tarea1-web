a38ca64536c9cabf6d3f7d03f92c92bdf12b4f76
Tomas <jtml.mass@gmail.com>
2019-09-26 01:51:16 -0400

    Completar el acapite D sobre cantidad de <form>
    Indicar la cantidad de formularios (form) que contiene el HTML por categorizando por el método implementado POST o GET

--- src/main/java/tarea1.java
+++ src/main/java/tarea1.java
@@ -17,9 +17,15 @@ public class tarea1 {
         //Parte B Indicar la cantidad de párrafos (p) que contiene el documento HTML
         Elements pElements = doc.select("p");
         System.out.println("Cantidad de Parrafos: " + pElements.size());
-        
+
         //Parte C Indicar la cantidad de imágenes (img) dentro de los párrafos que contiene el archivo HTML
         Elements imgElements = doc.select("p img");
         System.out.println("Cantidad de Imagenes dentro de Parrafos son: " + imgElements.size());
+
+        //Parte D indicar la cantidad de formularios (form) que contiene el HTML por categorizando por el método implementado POST o GET
+        Elements formElementsGet = doc.select("form[method~=get]");
+        System.out.println("Cantidad de Form elements por Get: " + formElementsGet.size());
+        Elements formElementsPost = doc.select("form[method~=post]");
+        System.out.println("Cantidad de Form elements por Post: " + formElementsPost.size());
     }
 }
