52c0be9734918a858f736eb33da58a693a9c4a10
Tomas <jtml.mass@gmail.com>
2019-09-26 15:16:24 -0400

    Reparar Acapite A con cantidad de Lineas y Acapite B con prueba de <img>

--- src/main/java/tarea1.java
+++ src/main/java/tarea1.java
@@ -1,3 +1,4 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,10 +13,10 @@ import static spark.Spark.post;
 
 public class tarea1 {
     public static void main(String[] args) throws IOException {
-        System.out.println("Ingrese la URL: ");
-        Scanner in = new Scanner(System.in);
-        String mainURL = in.nextLine();
-
+        //System.out.println("Ingrese la URL: ");
+        //Scanner in = new Scanner(System.in);
+        //String mainURL = in.nextLine();
+        String mainURL = "http://itachi.avathartech.com:4567/opcion2.html";
         boolean valid = Jsoup.isValid(Jsoup.connect(mainURL).get().html(), Whitelist.basic());
         if(valid){
             System.out.println("La URL ingresada no es valida. " + valid);
@@ -24,7 +25,9 @@ public class tarea1 {
             System.out.println("Titulo: " + doc.title());
 
             //Parte A Indicar la cantidad de lineas del recurso retornado
-            int lineas = doc.html().split(System.getProperty("line.separator")).length;
+            Connection.Response documentoRespuesta = Jsoup.connect(mainURL).execute();
+            String cuerpoDeRespuesta = documentoRespuesta.body();
+            int lineas = cuerpoDeRespuesta.split(System.getProperty("line.separator")).length;
             System.out.println("Cantidad de lineas en el Recurso retornado: " + lineas);
 
             //Parte B Indicar la cantidad de párrafos (p) que contiene el documento HTML
@@ -32,7 +35,7 @@ public class tarea1 {
             System.out.println("Cantidad de Parrafos: " + pElements.size());
 
             //Parte C Indicar la cantidad de imágenes (img) dentro de los párrafos que contiene el archivo HTML
-            Elements imgElements = doc.select("p img[src~=(?i)\\\\.(png|jpe?g|gif)]");
+            Elements imgElements = doc.select("p img");
             System.out.println("Cantidad de Imagenes dentro de Parrafos son: " + imgElements.size());
 
             //Parte D indicar la cantidad de formularios (form) que contiene el HTML por categorizando por el método implementado POST o GET
